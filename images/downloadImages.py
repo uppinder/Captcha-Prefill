c5ad233ee6f8950eab7607fd1c69cf3867e373ae
Uppinder <uppinder@iitg.ernet.in>
2016-09-29 03:13:40 +0530

    Automate image downloads.

---
+++ images/downloadImages.py
@@ -0,0 +1,11 @@
+import urllib,os
+from PIL import Image
+
+i = 20
+download_link = "https://webmail.iitg.ernet.in/plugins/captcha/backends/watercap/image_generator.php?sq=1475095334"
+while i>0:
+	imageName = str(i) + ".jpg"
+	urllib.urlretrieve(download_link,imageName)
+	Image.open(imageName).save(str(i)+".png","PNG")
+	os.remove(imageName)
+	i = i-1
--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function loadImages() {
+	for(i = 1;i <= 20;i++) {
+		var elem = '<img src="images/' + i.toString() + '.png" id="img' + i.toString() + '" onload="onLoadImage(this);">&nbsp;&nbsp;<canvas width="252" height="65" id="canvas-img' + i.toString()+ '"></canvas></br>'
+		$(document.body).prepend(elem);
+	}
+}
+
 function onLoadImage(img) {
 
 	var canvasId = 'canvas-img' + img.id.slice(-1);
@@ -6,7 +13,7 @@ function onLoadImage(img) {
 	ctx.drawImage(img,0,0);
 	
 	var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-	console.log(imageData);
+	//console.log(imageData);
 
 	var data = imageData.data;
 
