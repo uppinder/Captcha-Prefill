40aa9c79332d750eaa69eefc9afb0093d167f9d5
abhinavprince <abhinav.prince2012@gmail.com>
2016-10-05 03:30:36 +0530

    Implemented median filtering algorithm

--- js/main.js
+++ js/main.js
@@ -5,43 +5,64 @@ function loadImages() {
 	}
 }
 
+/***
+ *
+ * Median filtering to remove
+ * salt and pepper noise
+ */
 function onLoadImage(img) {
 
 	var canvasId = 'canvas-img' + img.id.slice(-1);
 	var canvas = document.getElementById(canvasId);
 	var ctx =  canvas.getContext('2d');
 	ctx.drawImage(img,0,0);
-	
-	var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-	// console.log(imageData);
 
-	var data = imageData.data;
-	// console.log(data);
-	
-	var T = 170;
-	/*
-	*	Median filtering to remove
-	*	salt and pepper noise.
-	*/
-	for(y = 0;y < canvas.height;++y) 
-		for(x = 0;x < canvas.width;++x) {
-			
-			var idx = (y*canvas.width+x)*4;
-			for(n = 0;n < 3;++n) {
-				var sum = 0;
-				for(j = y-1;j <= y+1;++j) { 
-					for(i = x-1;i <= x+1;++i) 
-						sum += data[(j*canvas.width+i)*4+n];
-				}
+    var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+    var newImageData = ctx.createImageData(imageData.width, imageData.height);
 
-				// sum -= data[idx+n];
-				var avg = sum/9;
-				avg = (avg < T?0:255);
-				
-				data[idx+n] = avg;	
-			}
-		}
+    for (x = 0; x < imageData.width; ++x) {
+        for(y = 0;y < imageData.height;++y) {
+            var idx = (y * imageData.width + x) * 4;
+            var avg = get_color(imageData.data, y, x, imageData.width);
+            newImageData.data[idx] = avg;
+            newImageData.data[idx + 1] = avg;
+            newImageData.data[idx + 2] = avg;
+            newImageData.data[idx + 3] = imageData.data[idx + 3];
+        }
+    }
 
-	ctx.putImageData(imageData,0,0);
+    // Repeat the same algorithm again to remove more noise
+    newImageData = remove_noise(newImageData, ctx);
 
+	ctx.putImageData(newImageData,0,0);
+
+}
+
+
+function get_color(data, Y, X, max_w) {
+    var sum = [], idx, n=0, x, y;
+    for(x = 0;x <= 3;++x) {
+        for (y = 0; y <= 3; ++y) {
+            idx = ( (Y + y) * max_w + (X + x) ) * 4;
+            sum[n++] = parseInt( parseFloat("0.33") *( parseFloat(data[idx]) + parseFloat(data[idx+1]) + parseFloat(data[idx+2])));
+        }
+    }
+    sum.sort();
+    return ((parseInt(sum[8] - sum[0]) < 210) && (parseInt(sum[4]) < 200)) ? 255 : 0;
+}
+
+
+function remove_noise(imageData, ctx) {
+    var newImageData = ctx.createImageData(imageData.width, imageData.height);
+    for (x = 0; x < imageData.width; ++x) {
+        for(y = 0;y < imageData.height;++y) {
+            var idx = (y * imageData.width + x) * 4;
+            var avg = get_color(imageData.data, y, x, imageData.width);
+            newImageData.data[idx] = avg;
+            newImageData.data[idx + 1] = avg;
+            newImageData.data[idx + 2] = avg;
+            newImageData.data[idx + 3] = imageData.data[idx + 3];
+        }
+    }
+    return newImageData;
 }
